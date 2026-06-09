d491d0531c994ee77368b75d9ea193664550724d
Dagmar87 <jdfssobrinho@hotmail.com>
2026-06-09 17:39:15 -0300

    Inicio do Projeto

---
+++ src/main/java/com/tarifa/TarifaAguaApplication.java
@@ -0,0 +1,13 @@
+package com.tarifa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarifaAguaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarifaAguaApplication.class, args);
+	}
+
+}
